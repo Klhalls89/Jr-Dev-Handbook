7579dc18eff6437aed5efb03cf7e2bc3e2b63632
Kristen Hallstrom <klhalls89@gmail.com>
2019-02-23 13:28:45 -0700

    inital commit

---
+++ src/data/handbook.js
@@ -0,0 +1,275 @@
+let handbook = [
+  { 
+    "question": "HTML stands for",
+    "answers": [
+            "Hypertext Markup Language",
+            "Hypertext Moxie Language",
+            "Hyperbolic Markup Language",
+            "Hypertext Markup Links"
+    ] 
+  },
+  { 
+    "question": "People access websites through software called a",
+    "answers": [
+                "Web browser",
+                "Web developer",
+                "Web researcher",
+                "Web controller"
+    ]
+  },
+  {
+    "question":  "Network request can be made with",
+    "answers": [
+                "All of these",
+                "A url",
+                "Script tags",
+                "Fetch calls" 
+    ]
+  },
+  { 
+    "question": "Network requests are made to a",
+    "answers": [
+              "Web server",
+              "Web browser",
+              "Server farm",
+              "Web controller"
+    ]
+  },
+  { 
+    "question": "Arguably the hardest thing about front-end dev is",
+    "answers": [
+              "Cross browser compatibility",
+              "Javascript",
+              "Hypertext Markup Language",
+              "Internet Explorer"
+    ]
+  },
+  { 
+    "question": "Which of the following tags are non-semantic HTML",
+    "answers": [
+                "<div> & <span>",
+                "<div> & <section>",
+                "<h1> & <span>",
+                "<p> & <form>"
+    ]
+  },
+  { 
+    "question": "Semantic HTML is",
+    "answers": [
+                "All of these",
+                "Good for developer empathy",
+                "Good for screen readers",
+                "Easily forgotten in React"
+    ]
+  },
+  { 
+    "question": "CSS stands for",
+    "answers": [
+                "Cascading Style Sheet",
+                "Computer Style Sheet",
+                "Computer Script System",
+                "Cascading Script System"
+    ]
+  },
+  {
+    "question": "SCSS stands for",
+    "answers": [
+                "Sassy Cascading Style Sheet",
+                "Stylish Computer Script Sheet",
+                "Sassy Computer Script System",
+                "Stylish Cascading Script System"
+    ]
+  },
+  {
+    "question": "Which of the following are true about SCSS",
+    "answers": [
+                "All of these",
+                "It’s SASS + CSS",
+                "It blends CSS with JS logic",
+                "It has less rigid syntax than SASS"
+    ]
+  },
+  {
+    "question": "Which of these is the de facto language of the internet",
+    "answers": [
+                "JavaScript",
+                "JSON",
+                "Java",
+                "CoffeeScript"
+    ]
+  },
+  {
+    "question": "What is the other name for JavaScripts",
+    "answers": [
+                "ECMAScript",
+                "JavaCoffee",
+                "CoffeeScript",
+                "Java"
+    ]
+  },
+  {
+    "question": "Which of the following are true about jQuery",
+    "answers": [
+                "All of these",
+                "It is a JS library",
+                "the syntax is shorter then JS",
+                "It does everything JS can do"
+    ]
+  },
+  {
+    "question": "What does DOM stand for",
+    "answers": [
+                "Document Object Model",
+                "Digital Object Model",
+                "Document Object Manipulation",
+                "Document Object Manipulation"
+    ]
+  },
+  { 
+    "question":  "React uses a special kind of DOM, called",
+    "answers": [
+                "A Virtual DOM",
+                "A Video DOM",
+                "A Reality DOM",
+                "A Viewing DOM"
+    ]
+  },
+  {
+    "question": "What is OOP",
+    "answers": [
+                "Object Oriented Programing",
+                "Object Oriented Protocol",
+                "Operation Object Programing",
+                "Optional Object Protocol"
+    ]
+  },
+  { "question": "What does AJAX stand for",
+    "answers": [
+                "Asynchronous JavaScript And XML",
+                "Algorithm JavaScript And XML ",
+                "Asynchronous jQuery And XML",
+                "Asynchronous JavaScript Also XL"
+    ]
+  },
+  {
+    "question": "XML, though slightly outdated, stands for",
+    "answers": [
+                "Extensible markup language",
+                "Exit markup language ",
+                "XL markup language",
+                "Existing markup language"
+    ]
+  },
+  { 
+    "question": "JS is executed",
+    "answers": [
+                "Synchronously",
+                "Asynchronously ",
+                "Congruently",
+                "Non-congruently"
+    ]
+  },
+  {
+    "question": "A fetch request is executed",
+    "answers": [
+                "Asynchronousl",
+                "Synchronously",
+                "Congruently",
+                "Non-congruently"
+    ]
+  },
+  { 
+    "question": "A fetch replaces what",
+    "answers": [
+                "XHR, Xml Http Requests",
+                "Xmling",
+                "HR requests",
+                "Hyper linking"
+    ]
+  },
+  {
+    "question": "JS classes are similar to these in React",
+    "answers": [
+                "Components",
+                "Props ",
+                "State",
+                "Data"
+    ]
+  },
+  {
+    "question": "JS classes are similar to these in React",
+    "answers": [
+                "Components",
+                "Props ",
+                "State",
+                "Data"
+    ]
+  },
+  { 
+    "question": "Classes and Components work best on SRP, which means",
+    "answers": [
+                "Single Responsibility Principle",
+                "Syntax Response Principle ",
+                "Single Responsibility Program",
+                "Single Response paradigm"
+    ]
+  },
+  {
+    "question": "The three basic languages of the internet are",
+    "answers": [
+                "HTML, CSS & JS",
+                "HTML, SCSS & JS ",
+                "HTML, CSS & jQuery",
+                "XML, CSS & JS"
+    ]
+  },
+  {
+    "question": "What is developer empathy",
+    "answers": [
+                "Writing easy to follow code for other devs",
+                "Know what other devs must be feeling ",
+                "Group think based programing",
+                "Feeling for the end user"
+    ]
+  },
+  { 
+    "question": "Who is a screen reader is used by",
+    "answers": [
+                "All of these",
+                "The legally blind ",
+                "People with learning disabilities",
+                "The illiterate"
+    ]
+  },
+  {
+    "question": "What direction does data flow in React",
+    "answers": [
+                "Down",
+                "Up",
+                "All direction",
+                "Sibling to Sibling"
+    ]
+  },
+  {
+    "question": "React was created by",
+    "answers": [
+                "Facebook",
+                "Twitter ",
+                "Instagram",
+                "Pinterest"
+    ]
+  },
+  { 
+    "question": "What does every react page need at the top",
+    "answers": [
+                "To import React",
+                "To import components from React ",
+                "To import React Dom",
+                "To import Data from React"
+    ]
+  }
+]
+
+module.exports = {
+    handbook  
+}
