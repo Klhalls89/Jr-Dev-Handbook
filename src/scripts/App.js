a044ad4625849696cbcddb159a18a72c0bda8298
Kristen Hallstrom <klhalls89@gmail.com>
2019-02-25 14:27:26 -0700

    validate answer add fetch call

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,19 +1,57 @@
 import React, { Component } from "react";
 import Card from "./Card";
-import handbook from "../data/handbook";
+// import handbook from "../data/handbook";
 
 class App extends Component {
  constructor(){
   super();
     this.state = {
-      questions: []
+      questions: [],
+      results: ''
     }
   }
 
-  componentDidMount() {
+  componentDidMount = () => {
+    if (!localStorage.getItem("incompleteCards")) {
+      this.fetchData() 
+  } else {
     this.setState({
-      questions: handbook
-    })  
+      questions: JSON.parse(localStorage.getItem("incompleteCards"))
+      })
+    }    
+  }
+
+  fetchData() {
+    fetch('http://memoize-datasets.herokuapp.com/api/v1/handbook')
+      .then(response => response.json())
+      .then(results => {
+        localStorage.setItem("incompleteCards", JSON.stringify(results.handbook));
+        this.setState({
+          questions: results.handbook
+        })
+      })
+      .catch(err => {
+        throw new Error(err);
+      }) 
+  }
+
+  useValidatedAnswer(ans) {
+    if (ans === 'correct') {
+      //change results to you got this
+      //shift current question out of the array
+      //after set state save question to local storage
+    } else {
+      invalidAnswer()
+    }
+  
+  }
+
+  invalidAnswer(){
+    //change results to try harder
+    //capture shifted question
+    //pop it to the end of the questions
+    //after set state save question to local storage
+
   }
 
   render() {
@@ -34,11 +72,11 @@ class App extends Component {
         Anything you get wrong will be asked again. Start studying!</p>
       </section>
       <section className="card-sect">
-        <Card currentQuestion={currentQuestion}/>
+       { currentQuestion && <Card useValidatedAnswer={this.useValidatedAnswer} currentQuestion={currentQuestion}/>}
       </section>
       <section className="results-sect">
         <h2>Results:</h2>
-        <p className="results">Correct</p>
+        <p className="results">{this.state.results}</p>
       </section>
     </div>
     )
--- src/scripts/Card.js
+++ src/scripts/Card.js
@@ -4,32 +4,50 @@ import React, { Component } from "react";
 class Card extends Component {
  constructor(){
   super();
+    this.state = {
+      correctAnswer:''
+    }
+  }
+
+ shuffleAnswers(arr) {
+   for (let i = arr.length - 1; i > 0; i--) {
+       const ranNum = Math.floor(Math.random() * (4));
+       [arr[i], arr[ranNum]] = [arr[ranNum], arr[i]];
+   }
+  }
+
+  getAnswers(arr) {
+    return arr.map((item) => {
+      return Object.values(item)[0]
+    })
+  }
+
+  populateAnswers(arr) {
+    return arr.map((answer) => {
+      return (<p onClick={this.validateAnswer}>{answer}</p>)
+    })
+  }
+
+  validateAnswer = (e) => {
+   if(e.target.innerText === this.props.currentQuestion.answers[0].Correct){
+    this.props.useValidatedAnswer('correct')
+   } else {
+    this.props.useValidatedAnswer('incorrect')
+   }
   }
 
   render() {
-    let displayedQueston;
-    let displayedAnswers;
-    let answer1;
-    let answer2;
-    let answer3;
-    let answer4;
+    let displayedQueston = this.props.currentQuestion.question
+    let answersToShuffle = [...this.props.currentQuestion.answers]
+    this.shuffleAnswers(answersToShuffle)
+    let answersText = this.getAnswers(answersToShuffle)
+    const allAnswers = this.populateAnswers(answersText)
 
-    if(this.props.currentQuestion){
-    displayedQueston = this.props.currentQuestion.question
-    displayedAnswers = this.props.currentQuestion.answers
-    answer1 = displayedAnswers[0].value;
-    answer2 = displayedAnswers[1].value;
-    answer3 = displayedAnswers[2].value;
-    answer4 = displayedAnswers[3].value;
-    }
-    
+
     return (
     <div className="card-div">
       <h3>{displayedQueston}</h3>
-      <p className="answer">{answer1}</p>
-      <p className="answer">{answer2}</p>
-      <p className="answer">{answer3}</p>
-      <p className="answer">{answer4}</p>
+      {allAnswers}
     </div>
     )
   }
